0962c7d6137a0f36547304e52872260c461338a2
Giuseppe <giuseppe.cristaudo@digitaslbi.com>
2016-11-24 14:08:48 +0100

    Implementata pagina deleteOrder

--- CarouselPageNavigation/AddOrderPage.xaml.cs
+++ CarouselPageNavigation/AddOrderPage.xaml.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace CarouselPageNavigation
 {
 	public partial class AddOrderPage : ContentPage
 	{
+		ObservableCollection<OrderDataModel> orders = OrderDataModel.All;
+
 		public AddOrderPage()
 		{
 			InitializeComponent();
 			ProductsList.ItemsSource = ProductsDataModel.All;
 			ProductsList.HasUnevenRows = true;
-			ProductsList.IsPullToRefreshEnabled = true;
+			//ProductsList.IsPullToRefreshEnabled = true;
 		}
 
 
@@ -19,14 +22,8 @@ namespace CarouselPageNavigation
 
 		public void OnMore(object sender, EventArgs e)
 		{
-			//var mi = ((MenuItem)sender);
+			orders.Add ((OrderDataModel)ProductsList.SelectedItem);
 			DisplayAlert("Prodotto Aggiunto!", "Adesso potrai effettuare il tuo ordine con un semplic click!", "OK");
 		}
-
-		//public void OnDelete(object sender, EventArgs e)
-		//{
-		//	var mi = ((MenuItem)sender);
-		//	DisplayAlert("Delete Context Action", mi.CommandParameter + " delete context action", "OK");
-		//}
 	}
 }
--- CarouselPageNavigation/DeleteOrderPage.xaml.cs
+++ CarouselPageNavigation/DeleteOrderPage.xaml.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace CarouselPageNavigation
 {
-	public partial class DeleteOrderPage : NavigationPage
+	public partial class DeleteOrderPage : ContentPage
 	{
+		ObservableCollection<OrderDataModel> orders = OrderDataModel.All;
+
 		public DeleteOrderPage()
 		{
 			InitializeComponent();
+			OrderList.ItemsSource = orders;
+			OrderList.HasUnevenRows = true;
+
+
+			//ProductsList.IsPullToRefreshEnabled = true;
+		}
+
+		public void OnDelete(object sender, EventArgs e)
+		{
+			orders.Remove((OrderDataModel)OrderList.SelectedItem);
+			DisplayAlert("Ordine rimosso!", "Puoi aggiungerne un altro cliccando su ADD", "OK");
 		}
 	}
 }
