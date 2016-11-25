75f42c1c5824a2a5dae346ef5513c154dec9dece
Giuseppe <giuseppe.cristaudo@digitaslbi.com>
2016-11-25 12:13:46 +0100

    Modifiche per gestione ordine con più prodotti

--- CarouselPageNavigation/AddOrderPage.xaml.cs
+++ CarouselPageNavigation/AddOrderPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
@@ -7,36 +8,43 @@ namespace CarouselPageNavigation
 	public partial class AddOrderPage : ContentPage
 	{
 		ObservableCollection<ProductsDataModel> all_products = ProductsDataModel.All;
-		ObservableCollection<OrderDataModel> orders = OrderDataModel.All;
-		ObservableCollection<ProductsDataModel> products = new ObservableCollection<ProductsDataModel>();
+		//ObservableCollection<OrderDataModel> orders = OrderDataModel.All;
+		//ObservableCollection<ProductsDataModel> products = new ObservableCollection<ProductsDataModel>();
 
 
 		public AddOrderPage()
 		{
-			foreach (ProductsDataModel p in all_products)
-			{
-				bool esiste = false;
-				foreach (OrderDataModel o in orders) {
-					if (p.Id == o.Product_id)
-					{
-						esiste = true;
-					}
-				}
-				if (!esiste)
-				{
-					products.Add(p); // Aggiungo il prodotto alla lista di prodotti da visualizzare.
-				}
-			}
+			//foreach (ProductsDataModel p in all_products)
+			//{
+			//	bool esiste = false;
+			//	foreach (OrderDataModel o in orders) {
+			//		if (p.Id == o.Product_id)
+			//		{
+			//			esiste = true;
+			//		}
+			//	}
+			//	if (!esiste)
+			//	{
+			//		products.Add(p); // Aggiungo il prodotto alla lista di prodotti da visualizzare.
+			//	}
+			//}
 
 			InitializeComponent();
-			ProductsList.ItemsSource = products;
+			ProductsList.ItemsSource = all_products;
 			ProductsList.HasUnevenRows = true;
+
 			//ProductsList.IsPullToRefreshEnabled = true;
 
 			//ProductsList.ItemTapped += this.OnAdd;
+
+			//ProductsList.ItemTapped += (object sender, ItemTappedEventArgs e) =>
+			//{
+			//	// don't do anything if we just de-selected the row
+			//	if (e.Item == null) return;
+			//	// do something with e.SelectedItem
+			//	((ListView)sender).SelectedItem = null; // de-select the row
+			//};
 		}
-
-
 
 
 
@@ -44,20 +52,30 @@ namespace CarouselPageNavigation
 		{
 			if (ProductsList.SelectedItem != null)
 			{
-				ProductsDataModel p = (ProductsDataModel)ProductsList.SelectedItem;
+				//Product p = (Product)ProductsList.SelectedItem;
+				ObservableCollection<Product> prodListToAdd = new ObservableCollection<Product>();
+				//prodListToAdd.Add(p);
 
-				OrderDataModel new_order =
-					new OrderDataModel
-					{
-						Img = p.Img,
-						Product_id = p.Id,
-						Name = p.Name,
-						Description = p.Description,
-						Color = p.Color
+				//OrderDataModel new_order =
+				//	new OrderDataModel
+				//	{
+				//		Id = Guid.NewGuid().ToString(),
+				//		orderName = "Other prod",
+				//		products = prodListToAdd
+				//	};
 
-					};
 
-				orders.Add(new_order);
+				//OrderDataModel new_order =
+				//	new OrderDataModel
+				//	{
+				//		Img = p.Img,
+				//		Product_id = p.Id,
+				//		Name = p.Name,
+				//		Description = p.Description,
+				//		Color = p.Color
+				//	};
+
+				//orders.Add(new_order);
 
 				DisplayAlert("Prodotto Aggiunto!", "Adesso potrai effettuare il tuo ordine con un semplic click!", "OK");
 			}
